e658b9718a9310d0ffd5d907b59e9ad5ae1f021d
Austin Kyker <amk66@duke.edu>
2018-05-23 08:46:08 -0400

    before using shared package

--- src/constants.ts
+++ src/constants.ts
@@ -1,19 +1,9 @@
-export const AuthHeaderField = 'x-access-token';
+// Constants only needed by the backend
+// Constants shared should go in shared npm package.
+
 export const JWTSecret = 'jwt_secret';
 
 export const DefaultGenreInterestLevel = 3;
 export const NumReviewsToBaseCLM = 3;
-
-export const MinQuestionsInQuiz = 5;
-export const MaxQuestionsInQuiz = 10;
-export const NumMultipleChoiceOptions = 5;
-export const MinNumPointsPerQuestion = 1;
-export const MaxNumPointsPerQuestion = 5;
-export const MaxQuestionPromptChars = 200;
-export const MaxLongAnswerReponseChars = 1000;
-
-export const MaxNumQuizAttempts = 3;
-export const MinHoursBetweenBookQuizAttempt = 0; // TODO: update to 24
-export const PassingQuizGrade = 70.0;
 
 export const HashedPassSaltLen = 8;
